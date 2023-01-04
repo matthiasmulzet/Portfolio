43b37f0d63bf44952fd6bca1e907092b2571773c
matthiasmulzet <mulzetmatthias@gmail.com>
2023-01-04 18:35:00 +0100

    added new component introduction

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { StartpageComponent } from './startpage/startpage.component';
+import { IntroductionComponent } from './introduction/introduction.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    StartpageComponent
+    StartpageComponent,
+    IntroductionComponent
   ],
   imports: [
     BrowserModule,

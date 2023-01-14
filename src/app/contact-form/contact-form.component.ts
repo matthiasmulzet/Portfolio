da2bd009a00e70ad609bc0fa88d0638ee5831ea7
matthiasmulzet <mulzetmatthias@gmail.com>
2023-01-14 17:45:46 +0100

    created overlay send mail div

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -1,4 +1,5 @@
-import { Component, ElementRef, ViewChild, ViewChildren, QueryList, Renderer2, HostListener } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+
 
 @Component({
   selector: 'app-contact-form',
@@ -8,17 +9,16 @@ import { Component, ElementRef, ViewChild, ViewChildren, QueryList, Renderer2, H
 export class ContactFormComponent {
 
   constructor() { }
-
   @ViewChild('inputRefName') inputName: HTMLInputElement | undefined;
   @ViewChild('inputRefEmail') inputEmail: HTMLInputElement | undefined;
   @ViewChild('textareaRefMessage') textareaRefMessage: HTMLTextAreaElement | undefined;
 
+  //Variables for send Mail
   @ViewChild('myForm') myForm!: ElementRef;
   @ViewChild('nameField') nameField!: ElementRef;
   @ViewChild('emailField') emailField!: ElementRef;
   @ViewChild('messageField') messageField!: ElementRef;
   @ViewChild('sendButton') sendButton!: ElementRef;
-  @ViewChild('errorNameSpan') errorNameSpan!: ElementRef;
 
   errorName: boolean = true;
   errorEmail: boolean = true;
@@ -32,6 +32,9 @@ export class ContactFormComponent {
   inputEmailInvalid: boolean | undefined;
   inputMessageInvalid: boolean | undefined;
 
+  hideOverlay: boolean = true;
+
+
   onFocus(inputRef: HTMLInputElement | HTMLTextAreaElement) {
     if (inputRef.id === 'name-field')
       this.firstInteractionNameLabel = true;
@@ -41,47 +44,55 @@ export class ContactFormComponent {
       this.firstInteractionMessageLabel = true;
   }
 
+
   onBlur(inputRef: HTMLInputElement | HTMLTextAreaElement) {
-    if (inputRef.id === 'name-field') {
-      let re = new RegExp("^([A-Za-zäöüÄÖÜß]{1,60})$");
-      if (re.test(inputRef.value)) {
-        this.errorName = true;
-        this.inputNameInvalid = false;
-      } else {
-        this.errorName = false;
-        this.inputNameInvalid = true;
-      }
-    }
+    if (inputRef.id === 'name-field')
+      this.checkInputNameValid(inputRef);
+    else if (inputRef.id === 'email-field')
+      this.checkInputEmailValid(inputRef);
+    else if (inputRef.id === 'message-field')
+      this.checkInputMessageValid(inputRef);
+  }
 
-    else if (inputRef.id === 'email-field') {
-      let re = new RegExp("^([a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,})$");
-      if (re.test(inputRef.value)) {
-        this.errorEmail = true;
-        this.inputEmailInvalid = false;
-      } else {
-        this.errorEmail = false;
-        this.inputEmailInvalid = true
-      }
-    }
 
-    else if (inputRef.id === 'message-field') {
-      let re = new RegExp("^.{1,}$");
-      if (re.test(inputRef.value)) {
-        this.errorMessage = true;
-        this.inputMessageInvalid = false;
-      } else {
-        this.errorMessage = false;
-        this.inputMessageInvalid = true;
-      }
+  checkInputNameValid(inputRef: HTMLInputElement | HTMLTextAreaElement) {
+    let re = new RegExp("^([A-Za-zäöüÄÖÜß]{1,60})$");
+    if (re.test(inputRef.value)) {
+      this.errorName = true;
+      this.inputNameInvalid = false;
+    } else {
+      this.errorName = false;
+      this.inputNameInvalid = true;
     }
   }
 
 
+  checkInputEmailValid(inputRef: HTMLInputElement | HTMLTextAreaElement) {
+    let re = new RegExp("^([a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,})$");
+    if (re.test(inputRef.value)) {
+      this.errorEmail = true;
+      this.inputEmailInvalid = false;
+    } else {
+      this.errorEmail = false;
+      this.inputEmailInvalid = true
+    }
+  }
+
+
+  checkInputMessageValid(inputRef: HTMLInputElement | HTMLTextAreaElement) {
+    let re = new RegExp("^.{1,}$");
+    if (re.test(inputRef.value)) {
+      this.errorMessage = true;
+      this.inputMessageInvalid = false;
+    } else {
+      this.errorMessage = false;
+      this.inputMessageInvalid = true;
+    }
+  }
+
 
   async sendMail() {
     if (!this.inputNameInvalid && !this.inputEmailInvalid && !this.inputMessageInvalid) {
-      //action="https://matthias-mulzet.at/send_mail/send_mail.php"
-      console.log('Sending mail', this.myForm);
       let nameField = this.nameField.nativeElement;
       let emailField = this.emailField.nativeElement;
       let messageField = this.messageField.nativeElement;
@@ -90,20 +101,17 @@ export class ContactFormComponent {
       emailField.disabled = true;
       messageField.disabled = true;
       sendButton.disabled = true;
-      //sende Animation anzeigen 
+      this.hideOverlay = false;
       let fd = new FormData();
       fd.append('name', nameField.value);
       fd.append('email', emailField.value);
       fd.append('message', messageField.value);
       await fetch('https://matthias-mulzet.at/send_mail/send_mail.php',
-        //https://matthias-mulzet.developerakademie.net/send_mail1/send_mail.php
         {
           method: 'POST',
           body: fd
         }
       );
-
-      //Text anzeigen Nachricht getestet
       nameField.disabled = false;
       emailField.disabled = false;
       messageField.disabled = false;
@@ -112,4 +120,10 @@ export class ContactFormComponent {
 
   }
 
+  removeOverlay() {
+    window.location.reload();
+    setTimeout(() => {
+      this.hideOverlay = true;
+    }, 300);
+  }
 }
